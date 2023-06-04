0b690a1014243772f99af8ff04cac55b085857a6
limonpro <imlimonpro@gmail.com>
2023-06-04 12:24:41 +0600

    contact details model add

--- src/components/ContactsModal/ContactsModal.jsx
+++ src/components/ContactsModal/ContactsModal.jsx
@@ -2,14 +2,44 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal'
 
+
+
+const ContactDetailsModal = ({ contact, handleClose }) => {
+
+    console.log(contact)
+     const { country } = contact;
+  const countryName = country ? country.name : '';
+    return (
+      <Modal show={true} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Contact Details</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Name: {countryName}</p>
+          <p>Phone: {contact.phone}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  };
+
 const ContactsModal = ({ hello }) => {
 
     const { show2, setShow2, handleClose2, handleShow2, area2, contacts2 , filteredContacts} = hello;
+    const [selectedContact, setSelectedContact] = useState(null);
 
-    // const [show2, setShow2] = useState(false);
-    // const handleClose2 = () => setShow2(false);
-    // const handleShow2 = () => setShow2(true);
-    // console.log(contacts2);
+    const handleContactClick = (contact) => {
+        setSelectedContact(contact);
+      };
+    
+      const handleCloseContactModal = () => {
+        setSelectedContact(null);
+      };
+
     console.log(area2);
     const textColor = area2 == 'All Contacts' ? '#46139f' : '#ff7f50'
 
@@ -24,11 +54,13 @@ const ContactsModal = ({ hello }) => {
                 <Modal.Header closeButton>
                     <Modal.Title style={{ color: textColor }}>{area2}</Modal.Title>
                 </Modal.Header>
-                <Modal.Body>
-                    {
-                        contacts2.map((contact, index) => <p key={index}>{contact.phone}</p>)
-                    }
-                </Modal.Body>
+               <Modal.Body>
+          {contacts2.map((contact, index) => (
+            <p key={index} onClick={() => handleContactClick(contact)}>
+              {contact.phone}
+            </p>
+          ))}
+        </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose2}>
                         Close
@@ -36,6 +68,9 @@ const ContactsModal = ({ hello }) => {
                     <Button variant="primary">Understood</Button>
                 </Modal.Footer>
             </Modal>
+            {selectedContact && (
+        <ContactDetailsModal contact={selectedContact} handleClose={handleCloseContactModal} />
+      )}
         </div>
     );
 };
